52589c4961e5a1ced55cdc022a9af9fff182672b
r2d <r2d@c51c8d5e-032a-db11-a0f2-0002b3467eef>
2007-07-22 13:42:10 +0000

    corrections for windows build

--- libmpc/trunk/libmpcpsy/cvd.c
+++ libmpc/trunk/libmpcpsy/cvd.c
@@ -16,11 +16,13 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-#include "mpc/mpc_types.h"
-#include "libmpcpsy.h"
 
 #include <math.h>
 #include <string.h>
+
+#include "mpc/mpc_types.h"
+#include "mpc/mpcmath.h"
+#include "libmpcpsy.h"
 
 void   Cepstrum2048  ( float* cep, const int );
 

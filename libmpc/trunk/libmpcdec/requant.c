01e7eef0c492775fbd26c53f5957b8205c6f27bd
r2d <r2d@c51c8d5e-032a-db11-a0f2-0002b3467eef>
2009-02-23 18:44:04 +0000

    update copyright (2nd part)

--- libmpc/trunk/libmpcdec/requant.c
+++ libmpc/trunk/libmpcdec/requant.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2005, The Musepack Development Team
+  Copyright (c) 2005-2009, The Musepack Development Team
   All rights reserved.
 
   Redistribution and use in source and binary forms, with or without

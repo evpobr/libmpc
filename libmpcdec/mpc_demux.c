784746bd71e4009695adbd545f310c962525c1b9
r2d <r2d@c51c8d5e-032a-db11-a0f2-0002b3467eef>
2012-01-01 16:05:19 +0000

    removed some gcc warnings and compilation issues

--- libmpcdec/mpc_demux.c
+++ libmpcdec/mpc_demux.c
@@ -78,16 +78,6 @@ static mpc_int32_t mpc_unread_bytes_unchecked(mpc_demux * d) {
 	return d->bytes_total + d->buffer - d->bits_reader.buff - ((8 - d->bits_reader.count) >> 3);
 }
 
-// Returns the amount of unread bytes in the demux buffer.
-static mpc_uint32_t mpc_unread_bytes(mpc_demux * d) {
-	mpc_int32_t unread_bytes = mpc_unread_bytes_unchecked(d);
-
-	if (unread_bytes < 0) return 0;
-	
-	return (mpc_uint32_t) unread_bytes;
-}
-
-
 
 // Returns the number of bytes available in the buffer.
 static mpc_uint32_t
@@ -96,8 +86,8 @@ mpc_demux_fill(mpc_demux * d, mpc_uint32_t min_bytes, int flags)
 	mpc_uint32_t unread_bytes = (mpc_uint32_t) mpc_unread_bytes_unchecked(d);
 	int offset = 0;
 
-	if ((mpc_int32_t)
-		unread_bytes < 0) return 0; // Error - we've been reading past the end of the buffer - abort
+	if ((mpc_int32_t) unread_bytes < 0)
+		return 0; // Error - we've been reading past the end of the buffer - abort
 
 	if (min_bytes == 0 || min_bytes > DEMUX_BUFFER_SIZE ||
 		    (unread_bytes < min_bytes && (flags & MPC_BUFFER_FULL) != 0 ))
